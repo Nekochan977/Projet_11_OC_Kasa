2ff35d4309c32daf3aa880e1a4a1df848d16f849
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-11-25 10:22:22 +0100

    component folder - rename app file + add header file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import '../styles/App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.png'
+
+function Header(){
+    return (
+        <header>
+            <img src={logo} alt="kasa logo"/>
+        </header>
+    )
+}
+
+export default Header
