280fd8f9faba7997229fae682789d9145eb0ebfb
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-11-25 12:04:58 +0100

    add bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,31 @@
-import logo from '../assets/logo.png'
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import '../styles/Header.css';
 
-function Header(){
-    return (
-        <header>
-            <img src={logo} alt="kasa logo"/>
-        </header>
-    )
+function Header() {
+  return (
+    <header className="container">
+      <div className="d-flex align-items-center container-fluid position-relative my-2">
+        <div>
+          <img src={logo} alt="kasa logo" />
+        </div>
+        <nav className="navbar position-absolute end-0 justify-content-center">
+          <ul className="d-flex navigation">
+            <li className="mx-5">
+              <a href="#" className="align-middle link">
+                Accueil
+              </a>
+            </li>
+            <li className="mx-5">
+              <a href="#" className="align-middle link">
+                A propos
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
