be5ff15255a91d94a1db9d9466ae7daa2ec2a6b9
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-11-25 16:57:39 +0100

    card-link

--- src/components/Card-link.jsx
+++ src/components/Card-link.jsx
@@ -1,6 +1,23 @@
-function CardLink (){
-    return(
-        <div>Card link</div>
-    )
+import data from "../utils/adds.json";
+import '../styles/Card-link.css'
+
+function CardLink() {
+  return (
+    <div className="container">
+      <div className="container-fluid bg-light rounded d-flex flex-wrap justify-content-md-around my-5">
+        {/* <h1>card</h1> */}
+        {/* {console.log(data)} */}
+        {data.map((data) => {
+          console.log(data);
+          return (
+            <div key={data.id} className="rounded bg-red-200 m-4 card cover-card">
+              <img className="thumbnail" src={data.cover} alt={data.title} />
+              <p>{data.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-export default CardLink
+export default CardLink;
