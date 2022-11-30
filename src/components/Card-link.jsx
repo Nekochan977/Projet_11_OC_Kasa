58c48bc087937de508d723fb3003abb59eadb771
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-11-30 21:59:39 +0100

    add images

--- src/components/Card-link.jsx
+++ src/components/Card-link.jsx
@@ -5,14 +5,12 @@ function CardLink() {
   return (
     <div className="container">
       <div className="container-fluid bg-light rounded d-flex flex-wrap justify-content-md-around my-5">
-        {/* <h1>card</h1> */}
-        {/* {console.log(data)} */}
         {data.map((data) => {
-          console.log(data);
+          //console.log(data);
           return (
-            <div key={data.id} className="rounded bg-red-200 m-4 card cover-card">
-              <img className="thumbnail" src={data.cover} alt={data.title} />
-              <p>{data.title}</p>
+            <div key={data.id} className="rounded bg-red-200 m-4  cover-card " >
+              <img className="thumbnail rounded position-relative bottom-0" src={data.cover} alt={data.title} />
+              <p className="position-absolute ">{data.title}</p>
             </div>
           );
         })}
