2852b6579bb9914dda51397d291be5f12b880730
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-11-30 22:29:28 +0100

    routes base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import '../styles/App.css';
 import Header from './Header';
 import Home from '../pages/Home';
+import About from '../pages/About';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' Pages={Home}/>
+          <Route index Pages={Home}> 
+          <Route path="/about" Pages={About} />
+        </Route>
+      </Routes>
       <Header />
       <Home />
     </div>
