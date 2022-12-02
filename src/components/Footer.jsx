7cddf9a484935d0feaace8a1fbd92e54374d01b2
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-02 15:06:40 +0100

    add text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import logoFooter from "../assets/logo-white.png"
 
 function Footer() {
     return(
-        <div className="w-full bg-dark align-items-center footer">
-            <img src={logoFooter} />
-        </div>
+        <footer className="d-flex flex-column w-full bg-dark align-items-center justify-content-center footer">
+            <img className="m-2" src={logoFooter} />
+            <p className="text-light">© 2020 Kasa. All rights reserved</p>
+        </footer>
     )
 }
 
