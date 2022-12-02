273e8cc75beced35ed9aecabbef510a79196af76
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-02 16:37:14 +0100

    footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import logoFooter from "../assets/logo-white.png"
 
+import '../styles/Footer.css'
+
 function Footer() {
     return(
-        <footer className="d-flex flex-column w-full bg-dark align-items-center justify-content-center footer">
+        <footer className="d-flex flex-column w-full bg-dark align-items-center justify-content-center footer mt-4">
             <img className="m-2" src={logoFooter} />
             <p className="text-light">© 2020 Kasa. All rights reserved</p>
         </footer>
