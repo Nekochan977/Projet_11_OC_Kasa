3ac526355a8a3a7e4a064d1c572a9256ddf3e04b
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-02 14:50:18 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import logoFooter from "../assets/logo-white.png"
+
+function Footer() {
+    return(
+        <div className="w-full bg-dark align-items-center footer">
+            <img src={logoFooter} />
+        </div>
+    )
+}
+
+export default Footer
