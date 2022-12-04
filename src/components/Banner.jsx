49c286b5e3faeb241a64a12c475f704d2027e1ba
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-04 15:49:44 +0100

    margin

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import '../styles/Banner.css';
 function Banner(props) {
     return(
         
-        <div className="container banner-container mt-8">
+        <div className="container banner-container my-8">
             <img className="rounded" src={props.source} alt={props.altText}/>
         </div>
 
