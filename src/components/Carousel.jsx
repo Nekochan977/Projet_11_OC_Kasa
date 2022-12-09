a850a777af1336a7d331563a15279583521bbbea
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 10:07:08 +0100

    carousel component base

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,16 @@
+function Carousel(props) {
+    const {children} = props
+    return(
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+                <div className="carousel-content-wrapper">
+                    <div className="carousel-content">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
