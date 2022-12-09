4beba4640a689f5343c1fdf4f123e5b1042dcc10
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 16:00:00 +0100

    appartment content + appartment css file

--- src/pages/Appartment.jsx
+++ src/pages/Appartment.jsx
@@ -1,25 +1,56 @@
 import { useParams } from "react-router-dom"
-import data from "../utils/adds.json";
+import data from "../utils/adds.json"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import Carousel from "../components/Carousel";
+import Carousel from "../components/Carousel"
+import Dropdown from "../components/Dropdown"
+import "../styles/Appartment.css"
 
 function Appartment(){
     const {appartmentId} = useParams()
     const appartment = data.find(appart => appart.id === appartmentId)
-    //const appartmentPictures = appartment.pictures
-    //console.log(appartment);
+    console.log(appartment)
     return (
       <div>
         <Header />
         <main className="container">
-          <h1>Appartment page</h1>
-          <h2>{appartment.title}</h2>
           <Carousel>
             {appartment.pictures.map((picture, index) => {
               return <img className="carousel-img" key={index} src={picture} />;
             })}
           </Carousel>
+          <section>
+            <div>
+              <div className="appartment-header">
+                <h1>{appartment.title}</h1>
+                <p>{appartment.location}</p>
+              </div>
+              <div className="host-info">
+                <div>
+                  <p>{appartment.host.name}</p>
+                </div>
+                <img
+                  className="host-profile-img"
+                  src={appartment.host.picture}
+                />
+              </div>
+            </div>
+            <div className="tags-rating">
+              <ul className="tags-list">
+                {appartment.tags.map((tag, index) => {
+                  return (
+                    <li className="tag-item" key={`${tag}-${index}`}>
+                      {tag}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="container mt-5 justify-content-center d-flex">
+              <Dropdown type={"Description"} text={appartment.description} />
+              <Dropdown type={"Équipements"} text={appartment.equipments} />
+            </div>
+          </section>
         </main>
         <Footer />
       </div>
