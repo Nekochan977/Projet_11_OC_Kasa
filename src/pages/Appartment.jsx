94423e57c3764dc345477e3b441bae7d5f751d66
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 14:08:53 +0100

    add carousel component and images

--- src/pages/Appartment.jsx
+++ src/pages/Appartment.jsx
@@ -2,20 +2,28 @@ import { useParams } from "react-router-dom"
 import data from "../utils/adds.json";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Carousel from "../components/Carousel";
 
 function Appartment(){
     const {appartmentId} = useParams()
     const appartment = data.find(appart => appart.id === appartmentId)
-    return(
-        <div>
-            <Header />
-            <main>
-                <h1>Appartment page</h1>
-                <h2>{appartment.title}</h2>
-            </main>
-            <Footer />
-        </div>
-    )
+    //const appartmentPictures = appartment.pictures
+    //console.log(appartment);
+    return (
+      <div>
+        <Header />
+        <main className="container">
+          <h1>Appartment page</h1>
+          <h2>{appartment.title}</h2>
+          <Carousel>
+            {appartment.pictures.map((picture, index) => {
+              return <img className="carousel-img" key={index} src={picture} />;
+            })}
+          </Carousel>
+        </main>
+        <Footer />
+      </div>
+    );
 }
 
 export default Appartment
