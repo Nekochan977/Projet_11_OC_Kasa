3121aee6dbefff83852312e65eab8420af6f5672
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 14:10:08 +0100

    add carousel style ans arrows

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,25 @@
+import "../styles/Carousel.css";
+
 function Carousel(props) {
-    const {children} = props
-    return(
-        <div className="carousel-container">
-            <div className="carousel-wrapper">
-                <div className="carousel-content-wrapper">
-                    <div className="carousel-content">
-                        {children}
-                    </div>
-                </div>
+    
+  const { children } = props;
+  return (
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        <button className="left-arrow">
+            &lt;
+        </button>
+        <div className="carousel-content-wrapper">
+            <div className="carousel-content mx-auto">
+                {children}
             </div>
         </div>
-    )
+        <button className="right-arrow">
+            &gt;
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
