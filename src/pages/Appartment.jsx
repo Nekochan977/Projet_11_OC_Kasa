5e5718e539405b2f916d27d63237b19958b34724
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 10:08:25 +0100

    Appartment HTML structure

--- src/pages/Appartment.jsx
+++ src/pages/Appartment.jsx
@@ -1,13 +1,19 @@
 import { useParams } from "react-router-dom"
 import data from "../utils/adds.json";
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 function Appartment(){
     const {appartmentId} = useParams()
     const appartment = data.find(appart => appart.id === appartmentId)
     return(
         <div>
-            <h1>Appartment page</h1>
-            <h2>{appartment.title}</h2>
+            <Header />
+            <main>
+                <h1>Appartment page</h1>
+                <h2>{appartment.title}</h2>
+            </main>
+            <Footer />
         </div>
     )
 }
