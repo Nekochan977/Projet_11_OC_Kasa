502c50df28f9d4a07ce9d042597a9605c04df1f1
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-09 16:17:00 +0100

    control buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,22 +1,58 @@
 import "../styles/Carousel.css";
+import { useState, useEffect } from "react";
 
 function Carousel(props) {
     
   const { children } = props;
+
+  // define a state for the current active index (currentIndex) and the total item that passed to the Carousel (length)
+const [currentIndex, setCurrentIndex] = useState(0)
+const [length, setLength] = useState(children.length)
+
+// Set the length to match current children from props
+useEffect(() => {
+    setLength(children.length)
+}, [children])
+// ...
+
+// function to handle when the control button is clicked
+const next = () => {
+    if (currentIndex < (length - 1)) {
+        setCurrentIndex(prevState => prevState + 1)
+    }
+}
+
+const prev = () => {
+    if (currentIndex > 0) {
+        setCurrentIndex(prevState => prevState - 1)
+    }
+}
+// ...
   return (
     <div className="carousel-container">
       <div className="carousel-wrapper">
-        <button className="left-arrow">
+      {/* condition to hide the control button when not needed */}
+        {currentIndex > 0 && (
+          <button onClick={prev} className="left-arrow">
             &lt;
-        </button>
+          </button>
+        )}
+       
         <div className="carousel-content-wrapper">
-            <div className="carousel-content mx-auto">
-                {children}
-            </div>
+          <div
+            className="carousel-content mx-auto"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {children}
+          </div>
         </div>
-        <button className="right-arrow">
+        {/* condition to hide the control button when not needed */}
+        {currentIndex < length - 1 && (
+          <button onClick={next} className="right-arrow">
             &gt;
-        </button>
+          </button>
+        )}
+       
       </div>
     </div>
   );
