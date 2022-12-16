7a7b2d3188416a13bf5f79f5f86ddd344c6cd8b4
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-16 15:13:50 +0100

    content + css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom"
+import "../styles/Error.css"
+
 function Error(){
 
     return(
-        <h1>Error page</h1>
+        <section className="my-container">
+            <div className="responsive">
+            <h1 className="error-title">404</h1>
+                <p className="">Oups! La page que vous demandez n'existe pas.</p>
+                <div>
+                    <Link
+                    to={`/`}>
+                        Retourner sur la page d’accueil
+                    </Link>
+                </div>
+            </div>
+        </section>
     )
 }
 
