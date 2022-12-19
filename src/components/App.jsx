5d082f127708b4bc7fc3d4374ecc9eded4dccf61
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-12-19 21:55:20 +0100

    add file routes.js + modify routes path in app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import '../styles/App.css'
-import Header from './Header'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Error from '../pages/Error'
 import { Routes, Route } from "react-router-dom"
 import Layout from './Layout'
 import Appartment from '../pages/Appartment'
+import { ABOUT, HOME } from '../routes'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path={HOME} element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path={ABOUT} element={<About />} />
           <Route path="appartment/:appartmentId" element={<Appartment />} />
           <Route path="*" element={<Error />} />
         </Route>
