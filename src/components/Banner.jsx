9a436245a3a75518a1b3d067189f98466179300e
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-01-03 14:06:16 +0100

    banner clean

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import '../styles/Banner.css';
 function Banner(props) {
     return(
         
-        <div className=" banner-container my-8 cover-banner">
+        <div className=" banner-container cover-banner">
             <img className="banner-image" src={props.source} alt={props.altText}/>
             <h1 className="banner-title">{props.title}</h1>
         </div>
