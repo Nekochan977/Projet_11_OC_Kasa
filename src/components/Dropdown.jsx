16fb29a2e93a73c841d343507fcf424338d4fa09
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-01-24 12:43:04 +0100

    simplified ternary

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,35 +5,27 @@ function DropDown(props) {
     const [open, setOpen] = useState(false)
     const handleOpen = () => {
         setOpen(!open)
-      }
+    }
     return (
       <div className="dropdown-container">
         <button className="dropdown-btn" onClick={handleOpen}>
           {props.type}
-          {open ? (
-            <i className="fa-solid fa-chevron-up"></i>
-          ) : (
-            <i className="fa-solid fa-chevron-down"></i>
-          )}
+          <i className={`fa-solid ${open ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
         </button>
-        {open ? (
+        {open && (
           <div className="dropdown-content active">
             {typeof props.text === "object" ? (
               <ul className="equipmentList">
-                {props.text.map((text, i) => {
-                  return (
-                    <li className="equipmentItem" key={text + i}>
-                      {text}
-                    </li>
-                  );
-                })}
+                {props.text.map((text, i) => (
+                  <li className="equipmentItem" key={text + i}>
+                    {text}
+                  </li>
+                ))}
               </ul>
             ) : (
               <p className="dropdown-text">{props.text}</p>
             )}
           </div>
-        ) : (
-          ""
         )}
       </div>
     );
